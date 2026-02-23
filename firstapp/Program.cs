0a59e988e0a367a2bc9ad273b54695eab87c583c
abhijeetsalvi <44025074+abhijeetsalvi@users.noreply.github.com>
2026-02-23 14:42:48 +0530

    Add files via upload

---
+++ firstapp/Program.cs
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+=======
+﻿using System;
+
+namespace firstapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! hi");
+        }
+    }
+}
+>>>>>>> ca78d86923ba769a9e9844e1022a69a4e30fa7d7
